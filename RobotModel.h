d9c8836cf984d127203a6c90e225455e5b8fca9e
douglashoang <dh4ang@gmail.com>
2016-10-03 18:18:50 -0500

    Made a robot class

---
+++ RobotModel.h
@@ -0,0 +1,16 @@
+#include <iostream.h>
+#include <string.h>
+
+using std::string;
+
+class RobotModel {
+	private:
+		string name;
+		int modelNumber;
+		double price;
+		// Add robot components
+
+
+	public:
+		
+};
