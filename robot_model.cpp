d8e820d39b04a4ba88f2ce6212c4451bdcc78d8f
gmd7266 <grantmdykstra@gmail.com>
2016-10-12 21:15:14 -0500

    Adding robot_model

--- robot_model.cpp
+++ robot_model.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <string>
 #include "robot_model.h"
+#include "robot_part.h"
+
+
+string RobotModel::getName() {
+    return this_name;
+}
+
+int RobotModel::getModelNumber() {
+    return this_modelNumber;
+}
+
+double RobotModel::getPrice() {
+    return this_price;
+}
+
+/*double RobotModel::componentCost() {
+
+}*/
+
+double RobotModel::maxSpeed() {
+    
+}
+
+void RobotModel::addPart(RobotPart piece) {
+    this_robotParts.push_back(piece);
+    this_price=this_price+piece.getCost();
+}
--- robot_model.h
+++ robot_model.h
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include "robot_part.h"
+
 
 using std::string;
 
 class RobotModel {
 	private:
-		string name;
-		int modelNumber;
-		double price;
-		// Add robot components
+		string this_name;
+		int this_modelNumber;
+		double this_price;
+		vector<RobotPart> *this_robotParts;
 
 
 	public:
+		RobotModel(string name, int modelNumber)
+			: this_name(name), this_modelNumber(modelNumber) {}
 		string getName();
+		int getModelNumber();
+		double getPrice();
+		//void componentCost();
+		double maxSpeed();
+		void addPart(RobotPart);
 };
